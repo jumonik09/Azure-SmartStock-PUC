8f7c90a390f37a12ca9dff943247a88bde2a7924
Juliana M <134242381+jumonik09@users.noreply.github.com>
2024-10-06 18:45:25 -0300

    login (parte front end)

---
+++ docs/codigo_fonte/login.js
@@ -0,0 +1,24 @@
+
+function login(event) {
+    event.preventDefault();
+    console.log("entrou na funcao login");
+
+    const email = document.getElementsByName("email")[0].value
+    const senha = document.getElementsByName("senha")[0].value
+    console.log("O email ingressado é " + email);
+    console.log("A senha ingressada é " + senha);
+
+    if (email == "" || senha == "") {
+        alert("Os campos email e senha são obrigatórios.");
+    } else if (!temArroba(email)) {
+        alert("Erro, o email não tem @");
+    } else {
+        console.log("Deveria agora navegar pro home");
+        alert("Login realizado com sucesso!");
+        window.location.href = "../home/index.html";
+    }
+
+}
+function temArroba(dado) {
+    return dado.includes('@')
+}
