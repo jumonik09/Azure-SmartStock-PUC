f328e81c6059ac0f2b8410c0dbd7cb367b5dce65
Juliana M <134242381+jumonik09@users.noreply.github.com>
2024-10-12 20:33:34 -0300

    Pagina front end - cadastrar produto e cadastrar fornecedor adicionei class Produto adicionei class Fornecedor

---
+++ src/Models/Fornecedor.cs
@@ -0,0 +1,9 @@
+public class Fornecedor{
+    public int id {get; set;}                                                     
+    public string razao {get;set;}
+    public string email {get;set;}
+    public string endereco {get;set;}
+    public  string telefone {get; set;}
+}
+
+   
