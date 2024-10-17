eaa071eeff3ed55001bf4a19002e4c5af643b206
Michael Benyamin Gorenstein Brasil <165437811+MichaelBrasil@users.noreply.github.com>
2024-10-17 19:32:56 -0300

    Update Produto.cs

--- src/Models/Produto.cs
+++ src/Models/Produto.cs
@@ -1,7 +1,11 @@
-public class Produto {
-    public int id {get; set;}
-    public string nome {get; set;}
-    public string categoria {get;set;}
-    public bool ativo {get; set;}
-    public int fornecedor_id {get;set;}
+namespace project.Models
+{
+    public class Produto
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string categoria { get; set; }
+        public bool ativo { get; set; }
+        public int fornecedor_id { get; set; }
+    }
 }
