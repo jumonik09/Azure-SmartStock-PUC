473c3f525a8a3c3e349b010d84fd0ae72bbcbc45
VictorMaglhs <victor.magalhaes@aglets.com.br>
2024-10-25 12:16:55 -0300

    feat: model de relação de ordem de compra e produto

---
+++ src/Models/OrdemDeCompraProduto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace project.Models
+{
+    public class OrdemDeCompraProduto
+    {
+        public int Id { get; set; }
+        public int OrdemDeCompraId { get; set; }
+        public int ProdutoId { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
