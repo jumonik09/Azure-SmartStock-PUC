f2e35b708f36a2b274f815a85195c754dcba0fa4
VictorMaglhs <victor.magalhaes@aglets.com.br>
2024-10-25 12:25:04 -0300

    feat: cadastro de uma ordem de compra

--- src/Controllers/OrdemDeCompraController.cs
+++ src/Controllers/OrdemDeCompraController.cs
@@ -4,6 +4,11 @@ using project.Models;
 
 namespace project.Controllers;
 
+public class ProdutosView
+{
+    public int Id { get; set; }
+    public int Quantidade { get; set; }
+}
 
 public class OrdemDeCompraController : Controller
 {
@@ -25,4 +30,37 @@ public class OrdemDeCompraController : Controller
         return View(model);
     }
 
+    [HttpPost]
+    public IActionResult CadastrarOrdemDeCompra(IFormCollection form)
+    {
+        var produtos = new List<ProdutosView>();
+        var fornecedor = int.Parse(form["fornecedor"]);
+        var usuario = int.Parse(form["usuario"]);
+        var total = float.Parse(form["total"]);
+
+        var OrdemDeCompra = new OrdemDeCompra { DataCriacao = DateTime.Today, FornecedorId = fornecedor, Status = "0", UsuarioId = usuario, Total = total };
+        _context.OrdemDeCompra.Add(OrdemDeCompra);
+
+        _context.SaveChanges();
+
+        int index = 0;
+        while (form[$"produtos[{index}][id]"].Count > 0)
+        {
+            int id = int.Parse(form[$"produtos[{index}][id]"]);
+            int quantidade = int.Parse(form[$"produtos[{index}][quantidade]"]);
+
+            produtos.Add(new ProdutosView { Id = id, Quantidade = quantidade });
+
+            index++;
+        }
+
+        foreach (var produto in produtos)
+        {
+            _context.OrdemDeCompraProduto.Add(new OrdemDeCompraProduto { OrdemDeCompraId = OrdemDeCompra.Id, ProdutoId = produto.Id, Quantidade = produto.Quantidade });
+        }
+
+        _context.SaveChanges();
+
+        return RedirectToAction("Sucesso", "Home");
+    }
 }
