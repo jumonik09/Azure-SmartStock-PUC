649efdd41e480709163e45c800b81a4c093637d4
VictorMaglhs <victor.magalhaes@aglets.com.br>
2024-10-25 12:16:35 -0300

    fix: colocando namespace no model

--- src/Models/Fornecedor.cs
+++ src/Models/Fornecedor.cs
@@ -1,9 +1,12 @@
-public class Fornecedor{
-    public int id {get; set;}                                                     
-    public string razao {get;set;}
-    public string email {get;set;}
-    public string endereco {get;set;}
-    public  string telefone {get; set;}
-}
+namespace project.Models
+{
+    public class Fornecedor
+    {
+        public int id { get; set; }
+        public string razao { get; set; }
+        public string email { get; set; }
+        public string endereco { get; set; }
+        public string telefone { get; set; }
 
-   
+    }
+}
