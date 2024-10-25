a787d843822e11cbe2ea7bcfa300548b17bba790
VictorMaglhs <victor.magalhaes@aglets.com.br>
2024-10-25 12:19:52 -0300

    feat: declaração dos models de ordem de compra

--- src/Controllers/ApplicationDbContext.cs
+++ src/Controllers/ApplicationDbContext.cs
@@ -3,13 +3,14 @@ using project.Models;
 
 public class ApplicationDbContext : DbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-    {
-    }
+  public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+  {
 
-    public DbSet<Fornecedor> Fornecedor { get; set; } 
+  }
 
-    public DbSet<Produto> Produto{ get; set; }
-
-    public DbSet<Usuario> Usuario { get; set; }
+  public DbSet<Fornecedor> Fornecedor { get; set; }
+  public DbSet<Produto> Produto { get; set; }
+  public DbSet<Usuario> Usuario { get; set; }
+  public DbSet<OrdemDeCompra> OrdemDeCompra { get; set; }
+  public DbSet<OrdemDeCompraProduto> OrdemDeCompraProduto { get; set; }
 }
