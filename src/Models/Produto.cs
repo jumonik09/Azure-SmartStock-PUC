196262235699757bf4788429a08d2c304bc8f580
VictorMaglhs <victor.magalhaes@aglets.com.br>
2024-10-25 12:18:06 -0300

    fix: validação para os valores que podem ser nulos

--- src/Models/Produto.cs
+++ src/Models/Produto.cs
@@ -3,8 +3,8 @@ namespace project.Models
     public class Produto
     {
         public int id { get; set; }
-        public string nome { get; set; }
-        public string categoria { get; set; }
+        public string? nome { get; set; }
+        public string? categoria { get; set; }
         public bool ativo { get; set; }
         public int fornecedor_id { get; set; }
         public float? preco { get; set; }
